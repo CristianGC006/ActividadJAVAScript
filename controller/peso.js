7565f323fddf0f3f8548d70e6831d96dba62550d
SeaScris <kalebparramarin135@gmail.com>
2024-11-07 18:31:11 -0500

    Se finalia el ejercicio de bases de datos

--- controller/peso.js
+++ controller/peso.js
@@ -1,7 +1,35 @@
-function conversionPeso(){
-    function gramosKilogramos(){}
-    function kilogramosLibras(){}
-    function librasGramos(){}
+export function conversionPeso(){
+    function gramosKilogramos(){
+        let conversionGramo = parseFloat(prompt("cuantos Kilogramos vas a ingresar: "));
+        let operacion =  conversionGramo/ 1000;
+        console.log(conversionGramo + " Equivale a " + operacion + " Kilogramos");
+    }
+    function kilogramosLibras(){
+        let conversionLibras = parseFloat(prompt("cuantos Libras vas a ingresar: "));
+        let operacion = conversionLibras * 0.453592;
+        if(conversionLibras<1){
+        console.log(conversionLibras + " Kilos Equivale a " + operacion + " Libras");
+      
+    }else{console.log(conversionLibras + " Kilo Equivale a " + operacion + " Libras");}}
+    function librasGramos(){
+        let conversionLibrasK = parseFloat(prompt("cuantos Libras vas a ingresar: "));
+        let operacion = conversionLibras * 453.592;
+        if(conversionLibrasK<1){
+        console.log(conversionLibrasK + "  Libra Equivale a " + operacion + " Gramos");
+    } else{console.log(conversionLibrasK + "  Libras Equivale a " + operacion + " Gramos");}}
 
-    // switch(opcione)
+    let opciones=Number(prompt("Elige una opcion:\n 1.Gramos a Kilogramos\n 2.Kilogramos a Libras\n 3.Libras a Gramos"))
+    switch(opciones){
+        case 1:
+            gramosKilogramos()
+            break;
+        case 2:
+            kilogramosLibras()
+            break;
+        case 3:
+            librasGramos()
+            break;
+        default:
+            console.log("Ingresa una opcion valida")
+    }
 }
--- views/public/js/index.js
+++ views/public/js/index.js
@@ -1,10 +1,12 @@
 import { conversionMetros_KM } from "../../../controller/longitud.js";
+import { conversionPeso } from "../../../controller/peso.js";
 let opcion=Number(prompt("Ingresa una opcion para:\n 1.Para calcular longitudes\n 2.Para calcular pesos"))
 switch(opcion){
     case 1:
         conversionMetros_KM()
         break;
     case 2:
+        conversionPeso()
         break;
     case 3:
      
